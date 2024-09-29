d3365c02a9e453557600d135ce56942b09f83260
den00333 <diniellec50@gmail.com>
2024-09-29 14:39:08 +0800

    user db

--- frontend/src/hooks/useCreateAcc.jsx
+++ frontend/src/hooks/useCreateAcc.jsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 export const useCreateAcc = () => {
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState(null)
-    const { userData } = useAuthContext()
+    const { user } = useAuthContext()
 
     const createAcc = async(userData) =>{   
         console.log('creating Account')
@@ -19,7 +19,8 @@ export const useCreateAcc = () => {
             // const uid = newUser.user.uid
             const newUser_token = await newUser.user.getIdToken();
             const data = {
-                role: userData.role
+                role: userData.role,
+                name: `${userData.firstname} ${userData.lastname}`
             }
             console.log(`userCreateAcc ${userData.role}`)
             const res = await axios.post('http://localhost:4000/admin/create', data, {
