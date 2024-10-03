97a1010fb7c7f5953c147f561f156b39efc091cd
den00333 <diniellec50@gmail.com>
2024-10-03 22:37:49 +0800

    useEffect on authrole

--- frontend/src/components/AuthRole.jsx
+++ frontend/src/components/AuthRole.jsx
@@ -1,19 +1,27 @@
 import { useAuthContext } from "../hooks/useAuthContext";
-import Login from "../AuthComponents/login";  
+import Login from "../AuthComponents/login";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const AuthRole = () => {
-    const navigate = useNavigate()
-    const {user} = useAuthContext();
-    if(!user){
-        return <Login/>
-    }else if(user.role === '1'){
+  const navigate = useNavigate();
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    if (user) {
+      if (user.role === '1') {
         navigate('/admin/dashboard');
-    }else if(user.role === '3') {
+      } else if (user.role === '3') {
         navigate('/operator/disbursementrecords');
-    }else if (user.role === '4'){
+      } else if (user.role === '4') {
         navigate('/editor/disbursementrecords');
-    }else{
-        return <div>Oopss</div>
+      }
     }
-}
+  }, [user, navigate]);
+
+  if (!user) {
+    return <Login />;
+  }
+
+  return <div>Oopss</div>;
+};
