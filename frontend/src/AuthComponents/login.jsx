2063e1230692f483859f3b84813cbf87df451082
AllenKirby <130225133+AllenKirby@users.noreply.github.com>
2024-10-14 22:05:42 +0800

    login with google added

--- frontend/src/AuthComponents/login.jsx
+++ frontend/src/AuthComponents/login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
 import bgImage from '../assets/images/NIAimg.png';
 import Loader from "../components/Loader";
-
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
     const [isChecked, setIsChecked] = useState(false)
@@ -16,44 +16,53 @@ const Login = () => {
     }
 
   return (
-    <form action="#" onSubmit={handleLogin} className="w-80 p-7 h-auto rounded-md shadow-md shadow-black bg-white">
-        <div className="w-full flex items-center justify-center py-3">
-            <img src={bgImage} alt="" className="w-20" />
+    <div className="flex flex-col">
+        <form action="#" onSubmit={handleLogin} className="w-80 p-7 h-auto rounded-md shadow-md shadow-black bg-white">
+            <div className="w-full flex items-center justify-center py-3">
+                <img src={bgImage} alt="" className="w-20" />
+            </div>
+            <div className="text-center mb-2">
+                <h1 className="text-2xl font-medium">Login</h1>
+            </div>
+            <div className="w-full flex items-center py-2 px-2 gap-2">
+                <input 
+                className="w-full peer z-[21] px-4 py-2 rounded-md outline-none duration-200 ring-2 ring-[transparent] focus:ring-customgreen" 
+                type="text" 
+                placeholder="Username" 
+                required 
+                onChange={(e) => setEmail(e.target.value)}/>
+            </div>
+            <div className="w-full flex items-center py-2 px-2 gap-2">
+                <input 
+                className="w-full peer z-[21] px-4 py-2 rounded-md outline-none duration-200 ring-2 ring-[transparent] focus:ring-customgreen" 
+                type={!isChecked ? 'password' : 'text'} 
+                placeholder="Password" 
+                required 
+                onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <div className="w-full flex gap-1 my-1 px-1"> 
+                <input type="checkbox" onClick={() => setIsChecked(!isChecked)}/>
+                <label className=" text-xs">Show Password</label>
+            </div>
+            <div className="w-full flex items-center justify-center py-3">
+                <button 
+                type="submit" 
+                disabled={isLoading} 
+                className="py-2 px-10 rounded-md bg-customgreen text-white hover:scale-125 transition-all duration-100"
+                >{isLoading ? <Loader/> : 'Login'}</button>
+            </div>
+            {error && (<div className="w-full text-center">
+                <h4 className="text-sm text-red-600">{error}</h4>
+            </div>)}
+            <div className="w-full h-auto px-2 pt-2 mt-5">
+            <div className="w-full h-auto px-2 pt-2 border-t-2 relative">
+                <p className="text-center bg-white rounded-lg px-3 absolute -top-3 left-[60px]">Or Login With</p>
+                <button className="w-full my-3 flex items-center justify-center py-2 rounded-lg border-2"><FcGoogle size={20} className="mx-2" />Google</button>
+            </div>
         </div>
-        <div className="text-center mb-2">
-            <h1 className="text-2xl font-medium">Login</h1>
-        </div>
-        <div className="w-full flex items-center py-2 px-2 gap-2">
-            <input 
-            className="w-full peer z-[21] px-4 py-2 rounded-md outline-none duration-200 ring-2 ring-[transparent] focus:ring-customgreen" 
-            type="text" 
-            placeholder="Username" 
-            required 
-            onChange={(e) => setEmail(e.target.value)}/>
-        </div>
-        <div className="w-full flex items-center py-2 px-2 gap-2">
-            <input 
-            className="w-full peer z-[21] px-4 py-2 rounded-md outline-none duration-200 ring-2 ring-[transparent] focus:ring-customgreen" 
-            type={!isChecked ? 'password' : 'text'} 
-            placeholder="Password" 
-            required 
-            onChange={(e) => setPassword(e.target.value)}/>
-        </div>
-        <div className="w-full flex gap-1 my-1 px-1"> 
-            <input type="checkbox" onClick={() => setIsChecked(!isChecked)}/>
-            <label className=" text-xs">Show Password</label>
-        </div>
-        <div className="w-full flex items-center justify-center py-3">
-            <button 
-            type="submit" 
-            disabled={isLoading} 
-            className="py-2 px-10 rounded-md bg-customgreen text-white hover:scale-125 transition-all duration-100"
-            >{isLoading ? <Loader/> : 'Login'}</button>
-        </div>
-        {error && (<div className="w-full text-center">
-            <h4 className="text-sm text-red-600">{error}</h4>
-        </div>)}
-    </form>
+        </form>
+        
+    </div>
   )
 }
 export default Login
