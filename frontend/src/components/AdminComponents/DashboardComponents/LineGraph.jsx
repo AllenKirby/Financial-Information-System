1a1914fcf7b6a3c7602080f76da18c87b72da296
den00333 <cordondinielle00333@gmail.com>
2024-11-14 13:32:30 +0800

    graphs

--- frontend/src/components/AdminComponents/DashboardComponents/LineGraph.jsx
+++ frontend/src/components/AdminComponents/DashboardComponents/LineGraph.jsx
@@ -5,13 +5,23 @@ import PropTypes from 'prop-types'
 const LineGraph = ({chartData}) => {
     const [year, setYear] = useState('2024');
 
+    const formatToPeso = (value) => {
+        return new Intl.NumberFormat('en-PH', {
+            style: 'currency',
+            currency: 'PHP',
+        }).format(value);
+    };
+
     const getYear = () => {
         const keys = chartData ? Object.keys(chartData) : [];
         return keys.sort((a, b) => b.localeCompare(a));
     };
 
     const [lineOptions, setLineOptions] = useState({
-        series: [{ name: 'Sales', data: [] }],
+        series: [
+            { name: 'Expense', data: [] },
+            { name: 'Forecasted Expense', data: [] }
+        ],
         options: {
             chart: {
                 height: 350,
@@ -21,19 +31,20 @@ const LineGraph = ({chartData}) => {
                     tools: { zoom: false, selection: false, pan: false, reset: false }
                 }
             },
-            stroke: { width: 5, curve: 'smooth' },
+            stroke: { width: [3, 3], curve: 'smooth', colors: ['#546E7A', '#FF5733'] },
             xaxis: { categories: [], tickAmount: 10 },
             title: { text: 'Time-Based Expense Overview', align: 'center', style: { fontSize: "16px", color: '#666' } },
-            fill: {
-                type: 'gradient',
-                gradient: {
-                    shade: 'dark',
-                    gradientToColors: ['#FDD835'],
-                    shadeIntensity: 1,
-                    type: 'horizontal',
-                    opacityFrom: 1,
-                    opacityTo: 1,
-                    stops: [0, 100, 100, 100]
+            grid : {
+                row: {
+                    color: ["#f3f3f3", "transparent"],
+                    opacity: 0.5
+                }
+            },
+            yaxis: {
+                labels: {
+                    formatter: function (value) {
+                        return value !== null ? formatToPeso(value.toFixed(2)) : ''; 
+                    }
                 }
             }
         }
@@ -44,16 +55,38 @@ const LineGraph = ({chartData}) => {
         const getvalues = () => (chartData[year] ? Object.values(chartData[year]) : []);
         const values = getvalues();
         const xAxis = getXAxis();
+        const forecastedData = JSON.parse(sessionStorage.getItem('forecasted')) || {};
+        const forecastXAxis = forecastedData.monthly ? Object.keys(forecastedData.monthly).map(date => date.slice(0, 7)) : [];
+        const forecastValues = forecastedData.monthly ? Object.values(forecastedData.monthly).map(data => data.forecast) : [];
+
+        const combinedXAxis = Array.from(new Set([...xAxis, ...forecastXAxis]));
+        console.log(combinedXAxis)
+
+        const actualData = combinedXAxis.map(date => {
+            const index = xAxis.indexOf(date);
+            return index !== -1 ? values[index] : null;
+        });
+
+        const forecastData = combinedXAxis.map(date => {
+            const index = forecastXAxis.indexOf(date);
+            return index !== -1 ? forecastValues[index] : null;
+        });
 
         setLineOptions((prevOptions) => ({
             ...prevOptions,
-            series: [{ ...prevOptions.series[0], data: values }],
-            options: { ...prevOptions.options, xaxis: { ...prevOptions.options.xaxis, categories: xAxis } }
+            series: [
+                { ...prevOptions.series[0], data: actualData },
+                { ...prevOptions.series[1], data: forecastData}    
+            ],
+            options: { 
+                ...prevOptions.options, 
+                xaxis: { ...prevOptions.options.xaxis, categories: combinedXAxis }
+            }
         }));
     }, [chartData, year]);
 
   return (
-    <div className="w-full h-1/2">
+    <div className="w-full h-full">
         <div className="flex items-center justify-end">
             <select value={year} onChange={(e) => setYear(e.target.value)}>
                 {getYear().map((year, index) => (
